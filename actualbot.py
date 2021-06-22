fe16c232a5fe6db91831b9de540891ecd4a95c07
AshDaGrat <77207196+AshDaGrat@users.noreply.github.com>
2021-06-22 16:05:22 +0530

    Update actualbot.py

--- actualbot.py
+++ actualbot.py
@@ -7,11 +7,9 @@ import random
 
 client = commands.Bot(command_prefix="!")
 
-
 @client.event
 async def on_ready():
     print('bot online')
-
 
 @client.command()
 async def beg(ctx):
@@ -21,34 +19,31 @@ async def beg(ctx):
     await adjust(ctx.author, 'wallet', begamt)
     await ctx.send(f"{person} just gave you {begamt} coins!")
 
-
 @client.command()
 async def test(ctx):
     data = await getdata()
     await ctx.send(data[str(ctx.author.id)]['wallet'])
 
-
 async def getdata():
     data = json.load(open('bank.json', 'r'))
     return data
 
-
 async def openaccount(user):
     data = await getdata()
-    if str(user.id) in data:
+    if str(user) in data:
         return False
     else:
-        data[str(user.id)] = {}
-        data[str(user.id)]['wallet'] = 100
-        data[str(user.id)]['bank'] = 0
-        data[str(user.id)]['stock'] = {'aapl': [0, 0],'goog': [0, 0],'nflx': [0, 0],'fb': [0, 0],'amzn': [0, 0],'tsla': [0, 0],'baba': [0, 0],'nvda': [0, 0],'msft': [0, 0],'twtr': [0, 0],'gme': [0, 0],'btc-usd': [0, 0],'bch-usd': [0, 0],'ltc-usd)': [0, 0],'eth-usd': [0, 0],'bnb-usd': [0, 0],'link-usd': [0, 0],'doge-usd': [0, 0]}
+        data[str(user)] = {}
+        data[str(user)]['wallet'] = 100
+        data[str(user)]['bank'] = 0
+        data[str(user)]['stock'] = {'aapl': [0, 0],'goog': [0, 0],'nflx': [0, 0],'fb': [0, 0],'amzn': [0, 0],'tsla': [0, 0],'baba': [0, 0],'nvda': [0, 0],'msft': [0, 0],'twtr': [0, 0],'gme': [0, 0],'btc-usd': [0, 0],'bch-usd': [0, 0],'ltc-usd)': [0, 0],'eth-usd': [0, 0],'bnb-usd': [0, 0],'link-usd': [0, 0],'doge-usd': [0, 0]}
     with open('bank.json', 'w') as bank:
         json.dump(data, bank, indent=4)
 
 async def adjust(user, acc, amt):
     await openaccount(user)
     data = await getdata()
-    data[str(user.id)][acc] += int(amt)
+    data[str(user)][acc] += int(amt)
     with open('bank.json', 'w') as bank:
         json.dump(data, bank, indent=4)
 
